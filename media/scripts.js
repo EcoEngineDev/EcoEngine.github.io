a0bf8a9756bd4edbe3341b5f441a2073594ae656
Kiwian <kiwiandoesthings@gmail.com>
2025-08-08 15:11:54 -0400

    better menu + all text = white now

--- media/scripts.js
+++ media/scripts.js
@@ -1,21 +1,24 @@
 // my beautiful fuck ass fix for mobile that simply changes properties of elements if were on mobile cuz i cant be FUCKED to do css
 
 console.log(navigator.userAgent);
-if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) { // we on mobile mf
-    console.log("we on mobile mf");
-    document.querySelectorAll(".viewer-container").forEach(element => {
-        element.style.display = "none";
-    });
-    document.getElementById("mobileNav").style.display = "none";
-    document.getElementById("navbar-dropdown").style.display = "block";
+
+function fixMobile() {
+    if (/Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent)) { // we on mobile mf
+        console.log("we on mobile mf");
+        document.getElementById("mobileNav").style.display = "none";
+        document.getElementById("navbar-dropdown").style.display = "block";
+    }
 }
 
+fixMobile();
+
 document.getElementById("navbar-dropdown").addEventListener("click", function () {
-    //if (document.getElementById("dropdown-bits").style.display === "block") {
-    //    document.getElementById("dropdown-bits").style.display = "none";
-    //} else {
-    //    document.getElementById("dropdown-bits").style.display = "block";
-    //}
-    //console.log("e");
     document.getElementById("dropdown-bits").classList.toggle("show");
 });
+
+document.querySelectorAll(".dropdown-item").forEach(element => {
+    element.addEventListener("click", function() {
+        document.getElementById("dropdown-bits").classList.remove("show");
+        fixMobile();
+    })
+})
